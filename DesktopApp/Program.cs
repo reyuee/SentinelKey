2653c9437ed82d123967092c949d61843901feef
rey <95544768+reyuee@users.noreply.github.com>
2026-06-05 20:44:35 +0200

    feat: read serial input from Arduino and check for key

---
+++ DesktopApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO.Ports;
+
+class Program
+{
+    static void Main()
+    {
+        SerialPort myPort = new SerialPort("COM9", 9600);
+        myPort.Open();
+
+        while (true)
+        {
+            string data = myPort.ReadLine();
+
+            if (data == "REY_INVISIBLE_KEY_2025+1")
+
+            {
+
+                Console.WriteLine("Auth Success");
+
+            }
+        }
+    }
+}
