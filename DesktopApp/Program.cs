3f0c2280b9934c910d30f85c91f6e05cf5e0ca7a
rey <95544768+reyuee@users.noreply.github.com>
2026-06-07 19:02:04 +0200

    feat: lock computer when arduino is unplugged
    If I start the program without the Arduino plugged in, it will crash because it can’t open the serial port.
    
    If the Arduino is connected the program reads data from it and checks if the secret key is correct from what the arudino is sending.
    
    If the key is correct it shows a success message.
    
    If the Arduino is unplugged after the program already started and the connection was working then it will lock the computer if the Arduino was unplugged

--- DesktopApp/Program.cs
+++ DesktopApp/Program.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Runtime.InteropServices;
 
 class Program
 {
+    [DllImport("user32.dll")]
+    public static extern bool LockWorkStation();
+
     static void Main()
     {
+        // change the COM port
+        // You can find the COM port in Device Manager or in the Arduino IDE
+
         SerialPort myPort = new SerialPort("COM13", 9600);
 
         myPort.DtrEnable = true;
@@ -16,19 +23,34 @@ class Program
 
         while (true)
         {
-            string data = myPort.ReadLine();
-
-            if (data.Trim() == "REY_INVISIBLE_KEY_2025+1")
+            try
             {
-                if (!isAuthenticated)
+                string data = myPort.ReadLine();
+
+                if (data.Trim() == "REY_INVISIBLE_KEY_2025+1")
                 {
-                    Console.ForegroundColor = ConsoleColor.Green;
+                    if (!isAuthenticated)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Auth Success");
+                        isAuthenticated = true;
 
-                    Console.WriteLine("Auth Success");
-
-                    isAuthenticated = true;
+                        
+                        Console.WriteLine("[AUTH] Valid key detected.");
+                    }
                 }
             }
+            catch
+            {
+                LockWorkStation();
+                break;
+
+            }
+
+          
+
+
+
         }
     }
 }
